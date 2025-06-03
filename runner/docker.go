9901ac8d45c7e2b26dd21e65a5858b285238477b
Merezha Roman <rmerezha@gmail.com>
2025-06-03 14:13:25 +0300

    Implement DockerRunner structure (stub methods)

---
+++ runner/docker.go
@@ -0,0 +1,43 @@
+package runner
+
+import (
+	"github.com/docker/docker/client"
+	"github.com/rmerezha/mtrpz-lab4/config"
+)
+
+type DockerRunner struct {
+	cli *client.Client
+}
+
+func NewDockerRunner() (*DockerRunner, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+	return &DockerRunner{cli: cli}, nil
+}
+
+func (d *DockerRunner) Run(c config.Container) error {
+	// TODO
+	return nil
+}
+
+func (d *DockerRunner) Stop(name string) error {
+	// TODO
+	return nil
+}
+
+func (d *DockerRunner) Kill(name string) error {
+	// TODO
+	return nil
+}
+
+func (d *DockerRunner) Restart(name string) error {
+	// TODO
+	return nil
+}
+
+func (d *DockerRunner) Remove(name string) error {
+	// TODO
+	return nil
+}
