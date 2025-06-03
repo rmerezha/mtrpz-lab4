1a577aa1684baa47cd95133bf09f86a2c4a9c994
Merezha Roman <rmerezha@gmail.com>
2025-06-03 14:27:17 +0300

    Implement Stop/Kill/Restart/Remove in DockerRunner

--- runner/docker.go
+++ runner/docker.go
@@ -1,9 +1,13 @@
 package runner
 
 import (
+	"context"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/rmerezha/mtrpz-lab4/config"
 )
+
+const SIGKILL = "SIGKILL"
 
 type DockerRunner struct {
 	cli *client.Client
@@ -23,23 +27,19 @@ func (d *DockerRunner) Run(c config.Container) error {
 }
 
 func (d *DockerRunner) Stop(name string) error {
-	// TODO
-	return nil
+	return d.cli.ContainerStop(context.Background(), name, container.StopOptions{})
 }
 
 func (d *DockerRunner) Kill(name string) error {
-	// TODO
-	return nil
+	return d.cli.ContainerKill(context.Background(), name, SIGKILL)
 }
 
 func (d *DockerRunner) Restart(name string) error {
-	// TODO
-	return nil
+	return d.cli.ContainerRestart(context.Background(), name, container.StopOptions{})
 }
 
 func (d *DockerRunner) Remove(name string) error {
-	// TODO
-	return nil
+	return d.cli.ContainerRemove(context.Background(), name, container.RemoveOptions{Force: true})
 }
 
 func (d *DockerRunner) PullImage(name string) error {
