19529edc08c83d9dcf8e957baeea752e3c20262b
Merezha Roman <rmerezha@gmail.com>
2025-06-04 21:41:09 +0300

    Implement cmd/slave

--- cmd/slave/main.go
+++ cmd/slave/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"flag"
+	"github.com/rmerezha/mtrpz-lab4/listener"
+	"github.com/rmerezha/mtrpz-lab4/runner"
+	"log"
+	"time"
+)
+
+var (
+	masterUrl = flag.String("master", "", "master node url")
+	host      = flag.String("host", "", "host node")
+	interval  = flag.Duration("interval", 5*time.Second, "interval")
+	token     = flag.String("token", "", "auth token")
+)
+
 func main() {
-	// TODO
+	flag.Parse()
+
+	if *masterUrl == "" {
+		log.Println("--master must be specified")
+	}
+	if *host == "" {
+		log.Println("--host must be specified")
+	}
+	if *token == "" {
+		log.Println("--token must be specified")
+	}
+
+	runner, err := runner.NewDockerRunner()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	globalListener := listener.GlobalListener{
+		Listeners: []listener.Listener{
+			listener.NewPollingListener(*masterUrl, *host, runner, *interval, *token),
+			listener.NewStateWatcherListener(*masterUrl, *host, runner, *interval, *token),
+		},
+	}
+
+	stopCh := make(chan struct{})
+	log.Println("slave node is starting")
+	globalListener.Listen(stopCh)
 }
